a4f087308def0d96b2d3d8f0637f1054f6ed5635
Yash Daxini <yash.daxini@tarktech.com>
2024-01-11 16:01:38 +0530

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Practice
+{
+    class HuffmanDecoding
+    {
+        //public String decode(string archive, string[] dictionary)
+        //{
+        //    Dictionary<string, int> map = new Dictionary<string, int>();
+        //    for (int i = 0; i < dictionary.Length; i++)
+        //    {
+        //        map.Add(dictionary[i], i+1);
+        //    }
+
+        //    StringBuilder cur = new StringBuilder();
+        //    StringBuilder ans = new StringBuilder();
+
+        //    for(int i = 0;i < archive.Length; i++) {
+        //        cur.Append(archive[i]);
+        //        if( map.ContainsKey(cur.ToString()))
+        //        {
+        //            ans.Append(map[cur.ToString()]);
+        //        }
+        //    }
+
+        //}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
